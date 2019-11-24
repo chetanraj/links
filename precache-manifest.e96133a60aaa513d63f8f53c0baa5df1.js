9ea6195bf1309f4603ea72cecbad34e3ed6bf7dd
Chetan Raj <chetan.raj@active.ai>
2019-11-24 21:25:26 +0530

    Updates

---
+++ precache-manifest.e96133a60aaa513d63f8f53c0baa5df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4993b13f36357091442ad0ef457cc907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe72c9113d5f45c0b19",
+    "url": "/static/css/main.2a853a35.chunk.css"
+  },
+  {
+    "revision": "c42d34cac37e0c529e88",
+    "url": "/static/js/2.542c71ed.chunk.js"
+  },
+  {
+    "revision": "abe72c9113d5f45c0b19",
+    "url": "/static/js/main.17587d9c.chunk.js"
+  },
+  {
+    "revision": "a34cb7faac4e539533ff",
+    "url": "/static/js/runtime-main.60741679.js"
+  }
+]);
