fbfc2bb3bfbb812de92ce16b557557f547d1bafb
Chetan Raj <chetan.narian@gmail.com>
2020-03-24 00:03:07 +0530

    Changes: Added design links and some changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <a className="user" rel="noopener noreferrer" href="https://www.instagram.com/thecjstream/">@thecjstream</a>
+      <img class="user-thumbnail" alt="Chetan Raj" src="https://avatars3.githubusercontent.com/u/1386629?s=460&amp;v=4"></img>
       <div className="links">
         <Link href="https://chetanraj.in" label="My portfolio" />
         <Link href="https://snpt.netlify.com/" label="Snpt" />
@@ -18,6 +18,7 @@ function App() {
         <Link href="https://twitter.com/chetan_raj" label="Twiiter" />
         <Link href="https://medium.com/jsessentials/build-a-restful-api-using-hapijs-and-mongoose-4903590c5663" label="Build a RESTful API using hapijs and mongoose" />
         <Link href="https://medium.com/jsessentials/how-gridsome-works-with-github-pages-aeacaf6f3622" label="How Gridsome works with GitHub Pages" />
+        <Link href="https://www.instagram.com/chetzdesigns/" label="Instagram [Web Design]" />
       </div>
     </>
   );
