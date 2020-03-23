59a84d7856e5cde27dc9c18455fe359f03f01e08
Chetan Raj <chetan.narian@gmail.com>
2020-03-24 00:05:58 +0530

    Updates

---
+++ precache-manifest.a55f5698c15e83468b4d556af50c11f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d87d305cd524ec730d5bff3a2a240d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e81116e9e2a435558a0",
+    "url": "./static/css/main.1e9545aa.chunk.css"
+  },
+  {
+    "revision": "c42d34cac37e0c529e88",
+    "url": "./static/js/2.542c71ed.chunk.js"
+  },
+  {
+    "revision": "1e81116e9e2a435558a0",
+    "url": "./static/js/main.384d95d1.chunk.js"
+  },
+  {
+    "revision": "583d4362dd3cab594a25",
+    "url": "./static/js/runtime-main.0ca9130f.js"
+  }
+]);
